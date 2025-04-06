ec248955006ea75f90ee3dcbe908c41afdcf8d8f
a-wholey <alexandriawholey@gmail.com>
2025-04-06 17:02:51 -0400

    Add player model

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
-    
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        #Limiting frame rate to 60 FPS
+        dt = clock.tick(60) / 1000
+        #Drawing player character
+        player.draw(screen)
         pygame.display.flip()
-
-
 
 if __name__ == "__main__":
     main()
