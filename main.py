78768a04b7dde3807455561bd63abd1d5cc1a238
a-wholey <alexandriawholey@gmail.com>
2025-04-06 17:40:56 -0400

    Add Player Movement and Rotation

--- main.py
+++ main.py
@@ -7,18 +7,28 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        #Drawing player character
+        updatable.update(dt)
+        
+        for obj in drawable:
+            obj.draw(screen)
+            
+        pygame.display.flip()
+
         #Limiting frame rate to 60 FPS
         dt = clock.tick(60) / 1000
-        #Drawing player character
-        player.draw(screen)
-        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
